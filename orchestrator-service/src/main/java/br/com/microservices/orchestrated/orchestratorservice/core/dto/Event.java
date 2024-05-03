65c0d269b5009ce5bd683628c3224189183427a7
Everton Martins <evertonmartins154@gmail.com>
2024-05-03 19:21:19 -0300

    Orchestrator-Service - add OrchestratorService

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Event.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Event.java
@@ -8,7 +8,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static sun.util.locale.LocaleUtils.isEmpty;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -31,4 +34,11 @@ public class Event {
     private List<History> eventHistory;
 
     private LocalDateTime createdAt;
+
+    public void addToHistory(History history){
+        if (isEmpty(eventHistory)){
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
